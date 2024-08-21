5cf91a10407c2051161656421ab44ee69ca19dca
Devang Salvi <devangsalvi21@gmail.com>
2024-08-21 08:00:22 +0000

    save script

---
+++ script6.js
@@ -0,0 +1,23 @@
+"use strict"
+
+function move(char, step) {
+const alpha = "abcdefghijklmnopqrstuvwxyz";
+const indexChar = alpha.indexOf(char);
+let finalIndex = indexChar + step;
+
+if (finalIndex > 25) {
+    finalIndex = finalIndex % 26;
+}
+
+const finalChar = alpha[finalIndex];
+return finalChar;
+
+//Find index of char
+// let index = alpha.indexOf(char);
+// index += 1;
+// console.log(alpha[index]);
+}
+const x = move ("z", 4);
+console.log(x);
+
+// move1("f");
